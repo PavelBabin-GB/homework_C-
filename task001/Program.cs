8bad3398eae4962d1ac50d80eeba7c34b6552da1
pavel <pavel-babin@yandex.ru>
2022-07-11 13:00:15 +0500

    Задача №1

---
+++ task001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу которая принимает на вход трехзначное число и на выход показывает последнюю цыфру этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    Console.WriteLine(number % 10 );
+}    
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
