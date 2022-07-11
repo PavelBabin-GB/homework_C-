1449d3a81c6aaf9f4d4336c5534d3815ef3f0828
pavel <pavel-babin@yandex.ru>
2022-07-11 13:15:32 +0500

    Задача №2

---
+++ task002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее. 
+Console.WriteLine("Введите первое число: ");
+int nomberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int nomberB = int.Parse(Console.ReadLine());
+if (nomberB > nomberA)
+{
+    Console.WriteLine("MAX = " + nomberB);
+    Console.WriteLine("Min = " + nomberA);
+}
+else
+{
+    Console.WriteLine("MAX = " + nomberA);
+    Console.WriteLine("Min = " + nomberB);
+}
